66e38421cf62227c6d189c735ea66bc5a93d9d8c
meghanroserebecca <meghanroserebecca@gmail.com>
2016-12-13 13:24:19 -0800

    minor alterations

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -14,15 +14,11 @@ function Article (opts) {
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
   $newArticle.attr('data-category', this.category);
-
-  /* TODO: Now use jQuery to fill in the rest of the current
-  template clone with properties from this particular Article instance.
-  We need to fill in:
-    1. author name,
-    2. author url,
-    3. article title
-    4. article body, and
-    5. publication date. */
+  $newArticle.attr('data-author name', this.author);
+  $newArticle.attr('data-author URL', this.authorURL);
+  $newArticle.attr('data-title', this.title);
+  $newArticle.attr('data-body', this.body);
+  $newArticle.attr('data-publication date', this.publishedOn);
 
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
