2659d9b96e81e429ba23df372932a0606d6c70fa
Clarefollett <chlaw101@gmail.com>
2016-12-13 15:28:04 -0800

    finished our todos

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -3,9 +3,9 @@ var articles = [];
 function Article (opts) {
   // TODO: Use the object passed in to complete this constructor function:
   // Save ALL the properties of `opts` into `this`.
-  this.author = opts.author;
   this.title = opts.title;
   this.category = opts.category;
+  this.author = opts.author;
   this.authorUrl = opts.authorUrl;
   this.publishedOn = opts.publishedOn;
   this.body = opts.body;
@@ -14,11 +14,10 @@ function Article (opts) {
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
   $newArticle.attr('data-category', this.category);
-  $newArticle.text('a', this.author);
-  $newArticle.attr('href', this.authorUrl);
-  $newArticle.text('h1', this.title);
-  $newArticle.text('article-body', this.body);
-  $newArticle.attr('datetime', this.publishedOn);
+  $newArticle.find('a').html(this.author);
+  $newArticle.find('a').attr('href', this.authorUrl);
+  $newArticle.find('h1').html(this.title);
+  $newArticle.find('section.article-body').html(this.body);
 
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
@@ -27,6 +26,7 @@ Article.prototype.toHtml = function() {
   as it now has real data attached to it! We need to account
   for that before this current article gets rendered to our
   DOM. */
+  $newArticle.removeAttr('class');
 
   return $newArticle;
 };
